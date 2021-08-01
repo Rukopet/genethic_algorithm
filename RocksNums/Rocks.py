d63acc4d8a87e6c95b6fead09911d0926897f698
egums <dima110.ru@gmail.com>
2021-08-01 15:16:30 +0300

    lock tuournament

--- RocksNums/Rocks.py
+++ RocksNums/Rocks.py
@@ -9,11 +9,11 @@ FITNESS_LIST_STANDARD = [50, 32, 232, 236]
 # FITNESS_LIST_STANDARD = [8, 12, 16, 20]
 # kek = [[2,2,2,2],[3,3,3,3],[4,4,4,4],[5,5,5,5]]
 
-POPULATION_SIZE = 200  # количество индивидуумов в популяции
+POPULATION_SIZE = 15000  # количество индивидуумов в популяции
 
 P_CROSSOVER = 0.9  # вероятность скрещивания
 P_MUTATION = 0.1  # вероятность мутации индивидуума
-MAX_GENERATIONS = 1000  # максимальное количество поколений
+MAX_GENERATIONS = 50  # максимальное количество поколений
 
 IN_DEEP = 5
 IN_WIDTH = 4
@@ -40,6 +40,9 @@ def clone(individ) -> List[list]:
     return deepcopy(individ)
 
 
+# def retBestInList(some_individs: list):
+
+
 def setTournament(population: list) -> List[list]:
     ret = []
     p_len = len(population)
@@ -49,13 +52,13 @@ def setTournament(population: list) -> List[list]:
             ind1, ind2, ind3 = random.randint(0, p_len - 1), random.randint(0, p_len - 1), random.randint(0,
                                                                                                           p_len - 1)
 
-        ret.append(deepcopy(max(population[ind1], population[ind2], population[ind3])))
+        ret.append(deepcopy(max([population[ind1], population[ind2], population[ind3]], key=getFitnessIndividual)))
     return ret
 
 
 def exchangeGenes(parent1: list, parent2: list):
     for i in range(len(parent1)):
-        if random.random() // (P_MUTATION % 100) == 0:
+        if random.random() < P_CROSSOVER:
             parent2[i], parent1[i] = parent1[i], parent2[i]
 
 
@@ -83,20 +86,21 @@ def main():
 
         for mutant in winner_tournament:
             if random.random() < P_MUTATION:
-                mutation(mutant, 1.0 / IN_DEEP * IN_WIDTH)
+                mutation(mutant, 1.0 / IN_DEEP)
 
         tmp_fit = getFitnessPopulation(winner_tournament)
         max_value, index_max_value = max([(v, i) for i, v in enumerate(tmp_fit)])
-        a.index(max(a))
+        # a.index(max(a))
 
-        mean = max_value / POPULATION_SIZE
+        mean = sum(tmp_fit) / POPULATION_SIZE
         maxFitnessValues.append(max_value)
         meanFitnessValues.append(mean)
         print(f"Поколение {iteration}: Макс приспособ. = {max_value}, Средняя приспособ.= {mean}")
-        print("Лучший индивидуум = ", *population[index_max_value], "\n")
+        print("Лучший индивидуум = ", *winner_tournament[index_max_value], "\n")
+        max_fittness = max_value if max_value > max_fittness else max_fittness
+        population = winner_tournament
         print(
             f"\t\t\t[ {sum(population[index_max_value][0])} ]\t[ {sum(population[index_max_value][1])} ]\t[ {sum(population[index_max_value][2])} ]\t[ {sum(population[index_max_value][3])} ]")
-
     plt.plot(maxFitnessValues, color='red')
     plt.plot(meanFitnessValues, color='green')
     plt.xlabel('Поколение')
